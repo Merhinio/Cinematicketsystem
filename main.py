cd20886d9b93f500343352debcc0a2d732144ee9
Mert <cm.sapci@gmail.com>
2024-08-18 15:40:22 +0200

    Kleines Kinoprogramm erstellt

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ Erwachsene = 10
 Senioren = 7.50
 
 frage = int(input('Wie alt bist du ? '))
+
+wieVieleTickets = int(input('Wie viele Tickets benötigst du ? '))
+
 if frage < 18:
-    print('Dein Kinder Ticket kostet', kinder, '€')
+    print('Dein Kinder Ticket kostet pro Person', kinder, '€, zusammen macht das ', wieVieleTickets * kinder, '€')
 elif 18 <= frage < 65:
-    print('Dein Erwachsener Ticket kostet', Erwachsene, '€')
+    print('Dein Erwachsener Ticket kostet pro Person', Erwachsene, '€ zusammen macht das ', wieVieleTickets * Erwachsene,'€')
 else:
-    print('dein Senioren Ticket kostet', Senioren, '€')
+    print('dein Senioren Ticket kostet pro Person', Senioren, '€ zusammen macht das ', wieVieleTickets * Senioren, '€')
 
 
